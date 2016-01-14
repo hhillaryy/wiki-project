3e3f7aab9784567cfb26225e2a25e9a6a384e9db
Hillary Hintzen and Steve Kinney <skinney103@gmail.com>
2016-01-14 15:08:31 -0800

    add new pages and update scripts and styles files

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,5 @@
 $(document).ready(function(){
-    var list = ["block" , "bootstrap", "css", "inline", "markdown" , "floats" , "jsvariables"];
+    var list = ["block" , "bootstrap", "css", "inline", "markdown" , "floats" , "jsvariables" , "jsmethods"];
     list.forEach(function(line) {
       $("#pages").append("<li><a href='pages/" + line + ".html'>" + line + "</a></li>");
     });
