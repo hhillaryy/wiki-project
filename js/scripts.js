b05f4b668dc80c9ad2735b731c94ed5557016585
Hillary Hintzen and Steve Kinney <hillary07@gmail.com>
2016-01-14 16:16:11 -0800

    complete forms page with small form update css and javascript

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,27 @@
 $(document).ready(function(){
-    var list = ["block" , "bootstrap", "css", "inline", "markdown" , "floats" , "jsvariables" , "jsmethods"];
+    var list = ["block" , "bootstrap", "css", "inline", "markdown" , "floats" , "jsvariables" , "jsmethods" , "forms"];
     list.forEach(function(line) {
       $("#pages").append("<li><a href='pages/" + line + ".html'>" + line + "</a></li>");
     });
+
+    $("#blank form").submit(function(event) {
+      var message1Input = $("input#message1").val();
+      var newMessage = message1Input.toUpperCase();
+
+      $(".message1").text(newMessage);
+
+      $("#output").show();
+      event.preventDefault();
+    });
 });
+
+
+  // $("#blah form").submit(function(event) {
+  //   var message1Input = $("input#message1").val();
+  //   var newMessage = message1Input.toUpperCase();
+  //
+  //   $(".message1").text(newMessage);
+  //
+  //   $("#output").show();
+  //   event.preventDefault();
+  // });
